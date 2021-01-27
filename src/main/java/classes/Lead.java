e80c0f66fbd554bd3966b5842de9b94a97fa8ba2
Javier Garcia <javiicc94@gmail.com>
2021-01-27 19:04:56 +0100

    Email validator created in a separated method

--- src/main/java/classes/Lead.java
+++ src/main/java/classes/Lead.java
@@ -2,9 +2,6 @@ package classes;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Lead {
     private int id;
     private String name;
@@ -12,7 +9,6 @@ public class Lead {
     private String email;
     private String companyName;
     private static int leadIdCounter = 1;
-    private static List<classes.Lead> leadList = new ArrayList<>();
 
     // When instantiating a Lead Object, it is automatically added to the List leadList.
     public Lead(String name, String phoneNumber, String email, String companyName) {
@@ -21,13 +17,18 @@ public class Lead {
         if (isPhoneNumberValid(phoneNumber)){
             this.phoneNumber = phoneNumber;
         }
-        if(EmailValidator.getInstance().isValid(email)){
+        if(isEmailValid(email)) {
             this.email = email;
-        } else{
-            throw new IllegalArgumentException("The email address format is not valid.");
         }
         this.companyName = companyName;
-        leadList.add(this);
+    }
+
+    public static boolean isEmailValid(String email){
+        if(!EmailValidator.getInstance().isValid(email)){
+            throw new IllegalArgumentException("The email address format is not valid.");
+        } else{
+            return true;
+        }
     }
 
     public static boolean isPhoneNumberValid(String phoneNumber){
@@ -43,13 +44,6 @@ public class Lead {
                         " 612345678 / 612-345-678 / 612 345 678 / 612 34 56 78");
             }
         return result;
-    }
-
-    public static void showLead(){
-        for(int i = 0; i<leadList.size();i++){
-            System.out.println("ID: " +leadList.get(i).getId() + " name: " + leadList.get(i).getName() + "\n");
-        }
-
     }
 
     public void printInfo(){
