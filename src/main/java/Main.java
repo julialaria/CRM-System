c112bb07afb089287c9abe918cd560974bd34679
Nerea Garcia <nereitagp12@gmail.com>
2021-01-28 21:29:21 +0100

    Codestyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,67 +18,46 @@ public class Main {
         Map<Integer, Opportunity> opportunities = new HashMap<>();
         Map<Integer, Account> accounts = new HashMap<>();
 
-        while (!order.toUpperCase().equals("EXIT")){
-            if(orderSplit.length>1){
-            if((order.toUpperCase().equals(keyPhrases[0]))){
+        while (!order.toUpperCase().equals("EXIT")) {
+            if (orderSplit.length > 1) {
+                if ((order.toUpperCase().equals(keyPhrases[0]))) {
+                    Lead lead = MainMethods.newLead();
+                    leads.put(lead.getId(), lead);
+                } else if (orderSplit[0].toUpperCase().equals(keyPhrases[1])) {
+                    try {
+                        int idLead = Integer.parseInt(orderSplit[1]);
+                        if (leads.containsKey(idLead) && orderSplit.length == 2) {
+                            Contact contact = MainMethods.convertLeadToContact(leads, idLead);
+                            contacts.put(contact.getId(), contact);
 
-                Lead lead = MainMethods.newLead();
-                leads.put(lead.getId(), lead);
+                            Opportunity opportunity = CreateOpportunity.create(contact);
+                            opportunities.put(opportunity.getId(), opportunity);
 
-            }
-            else if (orderSplit[0].toUpperCase().equals(keyPhrases[1])){
-                try{
-                    int idLead = Integer.parseInt(orderSplit[1]);
+                            Account account = CreateAccount.create(contact, opportunity);
+                            accounts.put(account.getId(), account);
 
-                    if (leads.containsKey(idLead) && orderSplit.length == 2){
-
-                        Contact contact = MainMethods.convertLeadToContact(leads, idLead);
-                        contacts.put(contact.getId(), contact);
-
-                        Opportunity opportunity = CreateOpportunity.create(contact);
-                        opportunities.put(opportunity.getId(), opportunity);
-
-                        Account account = CreateAccount.create(contact,opportunity);
-                        accounts.put(account.getId(),account);
-
-                        leads.remove(idLead);}
-
-                    else{
-                        System.out.println(ConsoleColors.RED +"This Lead id is not valid, please check Leads with command 'SHOW LEADS'");
+                            leads.remove(idLead);
+                        } else {
+                            System.out.println(ConsoleColors.RED + "This Lead id is not valid, please check Leads with command 'SHOW LEADS'");
+                        }
+                    } catch (NumberFormatException ex) {
+                        System.err.println(ConsoleColors.RED + "Introduce a valid Lead id");
                     }
-                }catch (NumberFormatException ex) {
-                    System.err.println(ConsoleColors.RED +"Introduce a valid Lead id");
+                } else if ((orderSplit[0] + " " + orderSplit[1]).toUpperCase().equals(keyPhrases[2]) && orderSplit.length > 2) {
+                    MainMethods.lookupOpportunity(orderSplit, opportunities);
+                } else if ((orderSplit[0] + " " + orderSplit[1]).toUpperCase().equals(keyPhrases[3]) && orderSplit.length > 2) {
+                    MainMethods.lookupLead(orderSplit, leads);
+                } else if (order.toUpperCase().equals(keyPhrases[4])) {
+                    MainMethods.showLeads(leads);
+                } else if (orderSplit[0].toUpperCase().equals(keyPhrases[5])) {
+                    MainMethods.closeLost(orderSplit, opportunities);
+                } else if (orderSplit[0].toUpperCase().equals(keyPhrases[6])) {
+                    MainMethods.closeWon(orderSplit, opportunities);
+                } else {
+                    System.out.println(ConsoleColors.RED + "COMAND NOT FOUND");
                 }
-
-            }
-            else if ((orderSplit[0]+" "+orderSplit[1]).toUpperCase().equals(keyPhrases[2])&&orderSplit.length>2){
-
-                MainMethods.lookupOpportunity(orderSplit, opportunities);
-            }
-
-            else if ((orderSplit[0]+" "+orderSplit[1]).toUpperCase().equals(keyPhrases[3])&&orderSplit.length>2){
-
-                MainMethods.lookupLead(orderSplit, leads);
-            }
-
-            else if (order.toUpperCase().equals(keyPhrases[4])){
-
-                MainMethods.showLeads(leads);
-
-            }
-            else if (orderSplit[0].toUpperCase().equals(keyPhrases[5])){
-
-                MainMethods.closeLost(orderSplit, opportunities);
-            }
-            else if (orderSplit[0].toUpperCase().equals(keyPhrases[6])){
-
-                MainMethods.closeWon(orderSplit, opportunities);
-            }
-            else {
-                System.out.println(ConsoleColors.RED +"COMAND NOT FOUND");
-            }}
-            else{
-                System.out.println(ConsoleColors.RED +"COMAND NOT FOUND");
+            } else {
+                System.out.println(ConsoleColors.RED + "COMAND NOT FOUND");
             }
             order = MainMethods.whatNext();
             orderSplit = order.split(" ");
