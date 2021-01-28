8951b257533ae02f91e1652166095387cb2bcb2c
ester-naranjo-rodrigo <esthernaran@gmail.com>
2021-01-28 15:29:12 +0100

    Main update

---
+++ src/test/java/classes/mainMethodsTest.java
@@ -0,0 +1,44 @@
+package classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mainMethodsTest {
+
+    @Test
+    void newLead() {
+    }
+
+    @Test
+    void convertLeadtoOpportunity() {
+    }
+
+    @Test
+    void convertLeadtoLead() {
+    }
+
+    @Test
+    void lookupOpportunity() {
+    }
+
+    @Test
+    void lookupLead() {
+    }
+
+    @Test
+    void showLeads() {
+    }
+
+    @Test
+    void closeLost() {
+    }
+
+    @Test
+    void closeWon() {
+    }
+
+    @Test
+    void whatNext() {
+    }
+}
